ac25d041620590e4c4efe648dc2a9f3fb2699e15
CampanaJF <jucampana@alumno.unlam.edu.ar>
2024-01-30 08:48:07 -0300

    init

---
+++ src/main/java/com/kfp/craftingrecipes/CraftingRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.kfp.craftingrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftingRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftingRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kfp/craftingrecipes/CraftingRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfp.craftingrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftingRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
