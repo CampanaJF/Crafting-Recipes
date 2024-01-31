9f215b919b0d518feebbc0e5a06b1431c9d14ea1
CampanaJF <jucampana@alumno.unlam.edu.ar>
2024-01-31 16:57:27 -0300

    added some tests

--- src/test/java/com/kfp/craftingrecipes/repository/RecipeJpaRepositoryTest.java
+++ src/test/java/com/kfp/craftingrecipes/repository/RecipeJpaRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.kfp.craftingrecipes.repository;
 
 import com.kfp.craftingrecipes.model.view.RecipeIngredientsProjection;
+import com.kfp.craftingrecipes.model.view.RecipeNameProjection;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -30,5 +33,39 @@ class RecipeJpaRepositoryTest {
         assertNotNull(recipeIngredients);
         assertEquals("Plans: Hammer of Righteous Might", recipeIngredients.getName());
     }
+
+    // H2 IS CASE SENSITIVE FOR THIS CASE, SO THE RESULT IS A LITTLE DIFFERENT
+    @Test
+    void shouldFindAllMatchingRecipesThatContainTheSearchedParameterInTheirName() {
+        List<RecipeNameProjection> recipes = recipeJpaRepositoryRepository.searchRecipesByName("Ha");
+
+        assertNotNull(recipes);
+        assertEquals(2, recipes.size());
+        assertEquals("Plans: Hammer of Righteous Might", recipes.get(0).getName());
+    }
+
+    @Test
+    void shouldFindAllMatchingRecipesThatHaveTheSearchedProfession() {
+        List<RecipeNameProjection> recipes = recipeJpaRepositoryRepository
+                .searchRecipesByProfession("Enchanting");
+
+        assertNotNull(recipes);
+        assertEquals(4, recipes.size());
+        assertEquals("Formula: Enchant Weapon - Mongoose", recipes.get(0).getName());
+    }
+
+    @Test
+    void shouldFindAllMatchingRecipesThatHaveTheSearchedRarity() {
+        List<RecipeNameProjection> recipes = recipeJpaRepositoryRepository
+                .searchRecipesByRarity("Common");
+
+        recipes.forEach(recipe -> System.out.println(recipe.getName()));
+
+        assertNotNull(recipes);
+        assertEquals(4, recipes.size());
+        assertEquals("Schematic: White Smoke Flare", recipes.get(0).getName());
+    }
+
+
 
 }
